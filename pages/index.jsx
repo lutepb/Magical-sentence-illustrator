8b9987c0b263b66f98cd30658cda4a92a8d409a7
lutepb <john.braa12@gmail.com>
2025-06-27 12:38:57 -0400

    Add missing pages and API route

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import MagicalSentenceApp from "../components/floral_sentence_local_illustration";
+import floral_sentence_local_illustration from "../components/floral_sentence_local_illustration";
 
 export default function Home() {
-  return <MagicalSentenceApp />;
+  return <floral_sentence_local_illustration />;
 }
