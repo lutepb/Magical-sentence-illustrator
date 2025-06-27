5bc492fd5da57865d5883d573608279da387132a
lutepb <john.braa12@gmail.com>
2025-06-27 12:31:16 -0400

    Connect working frontend with OpenAI backend image generation

---
+++ pages/index.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import MagicalSentenceApp from "../components/floral_sentence_local_illustration";
+
+export default function Home() {
+  return <MagicalSentenceApp />;
+}
